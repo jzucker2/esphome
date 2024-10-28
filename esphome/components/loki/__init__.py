441f19bae55c0237f29dc9ebd7c3e78a0d491360
Jordan Zucker <jordan.zucker@gmail.com>
2024-10-28 10:41:16 -0700

    Added a codeowner too

--- esphome/components/loki/__init__.py
+++ esphome/components/loki/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
+CODEOWNERS = ["@jzucker2"]
 DEPENDENCIES = ["network"]
 
 loki_component_ns = cg.esphome_ns.namespace("loki")
