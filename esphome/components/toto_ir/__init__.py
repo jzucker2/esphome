bb6e5e7a0ac475371032f899d68bd0952a6b259e
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-09 15:03:51 -0800

    Hoping that's a little better

--- esphome/components/toto_ir/__init__.py
+++ esphome/components/toto_ir/__init__.py
@@ -1,21 +1,29 @@
 import esphome.codegen as cg
+from esphome.components import remote_base
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
+DEPENDENCIES = ["remote_transmitter"]
+AUTO_LOAD = ["remote_base"]
 CODEOWNERS = ["@jzucker2"]
 
 CONF_TOTO_ID = "toto_id"
 
 toto_ir_ns = cg.esphome_ns.namespace("toto_ir")
-TotoIR = toto_ir_ns.class_("TotoIR", cg.Component)
+TotoIR = toto_ir_ns.class_("TotoIR", cg.Component, remote_base.RemoteTransmittable)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(TotoIR),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(TotoIR),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(remote_base.REMOTE_TRANSMITTABLE_SCHEMA)
+)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await remote_base.register_transmittable(var, config)
