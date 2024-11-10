e90edc9ac94792d7feaf36e1695f6d64a1ad743c
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-10 12:45:12 -0800

    Trying again but rolling back and including upstream dev latest

--- esphome/components/toto_ir/toto_ir.h
+++ esphome/components/toto_ir/toto_ir.h
@@ -3,13 +3,12 @@
 #include "esphome/core/component.h"
 #include "esphome/components/remote_base/remote_base.h"
 #include "esphome/components/remote_transmitter/remote_transmitter.h"
-#include "toto_ir_codes.h"
 
 namespace esphome {
 namespace toto_ir {
 
 using remote_base::RemoteTransmitterBase;
-using remote_base::TotoIRCodes::RawTimings;
+using remote_base::RawTimings;
 
 class TotoIR : public Component, public remote_base::RemoteTransmittable {
  public:
@@ -17,7 +16,7 @@ class TotoIR : public Component, public remote_base::RemoteTransmittable {
   void loop() override;
   void dump_config() override;
   // general functions
-  void transmit_(remote_base::TotoIRCodes::RawTimings &ir_code);
+  void transmit_(RawTimings &ir_code);
   // functions
   void send_power_toggle();
   void send_rear_wash();
