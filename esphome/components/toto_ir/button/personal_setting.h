3973bf960eb5d1debe6a8684ba03c573be3bc7bd
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-10 21:36:05 -0800

    Hoping that's good enough to compile

--- esphome/components/toto_ir/button/personal_setting.h
+++ esphome/components/toto_ir/button/personal_setting.h
@@ -9,9 +9,12 @@ namespace toto_ir {
 class PersonalSettingButton : public button::Button, public Parented<TotoIR> {
  public:
   PersonalSettingButton() = default;
+  void set_water_pressure(const std::string &water_pressure) { this->water_pressure_ = water_pressure; }
+  std::string get_water_pressure_() const { return this->water_pressure_; }
 
  protected:
   void press_action() override;
+  std::string water_pressure_{};
 };
 
 }  // namespace toto_ir
