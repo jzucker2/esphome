3065529c704dbe680e584de0685df73b93e3cffb
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-10 22:07:10 -0800

    I think I got it now

--- esphome/components/toto_ir/button/personal_setting.cpp
+++ esphome/components/toto_ir/button/personal_setting.cpp
@@ -8,10 +8,10 @@ static const char *const TAG = "toto_ir.personal_setting_button";
 void PersonalSettingButton::press_action() {
   ESP_LOGI(TAG, "Sending personal setting request");
   this->parent_->set_water_pressure(this->get_water_pressure());
-  this->parent_->set_water_position("5");
-  this->parent_->set_water_temperature("3");
-  this->parent_->set_seat_temperature("3");
-  this->parent_->set_fan_temperature("3");
+  this->parent_->set_water_position(this->get_water_position());
+  this->parent_->set_water_temperature(this->get_water_temperature());
+  this->parent_->set_seat_temperature(this->get_seat_temperature());
+  this->parent_->set_fan_temperature(this->get_fan_temperature());
 }
 
 }  // namespace toto_ir
--- esphome/components/toto_ir/button/personal_setting.h
+++ esphome/components/toto_ir/button/personal_setting.h
@@ -11,10 +11,22 @@ class PersonalSettingButton : public button::Button, public Parented<TotoIR> {
   PersonalSettingButton() = default;
   void set_water_pressure(const std::string &water_pressure) { this->water_pressure_ = water_pressure; }
   std::string get_water_pressure() const { return this->water_pressure_; }
+  void set_water_position(const std::string &water_position) { this->water_position_ = water_position; }
+  std::string get_water_position() const { return this->water_position_; }
+  void set_water_temperature(const std::string &water_temperature) { this->water_temperature_ = water_temperature; }
+  std::string get_water_temperature() const { return this->water_temperature_; }
+  void set_fan_temperature(const std::string &fan_temperature) { this->fan_temperature_ = fan_temperature; }
+  std::string get_fan_temperature() const { return this->fan_temperature_; }
+  void set_seat_temperature(const std::string &seat_temperature) { this->seat_temperature_ = seat_temperature; }
+  std::string get_seat_temperature() const { return this->seat_temperature_; }
 
  protected:
   void press_action() override;
   std::string water_pressure_{};
+  std::string water_position_{};
+  std::string water_temperature_{};
+  std::string fan_temperature_{};
+  std::string seat_temperature_{};
 };
 
 }  // namespace toto_ir
