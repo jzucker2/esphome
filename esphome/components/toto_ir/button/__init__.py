84795121c96346bf68612437c60637317e68be40
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-10 21:59:49 -0800

    This is great

--- esphome/components/toto_ir/button/__init__.py
+++ esphome/components/toto_ir/button/__init__.py
@@ -104,5 +104,8 @@ async def to_code(config):
         if conf := config.get(button_type):
             btn = await button.new_button(conf)
             await cg.register_parented(btn, config[CONF_TOTO_ID])
+            # for key in [x for x in enc_conf if x in KEYPAD_KEYS]:
+            #     b_sensor = await cg.get_variable(enc_conf[key])
+            #     cg.add(listener.add_button(b_sensor, literal(f"LV_KEY_{key.upper()}")))
             if button_type == CONF_PERSONAL_SETTING:
-                cg.add(btn.set_water_pressure(config[CONF_WATER_PRESSURE_OPTION]))
+                cg.add(btn.set_water_pressure(conf[CONF_WATER_PRESSURE_OPTION]))
