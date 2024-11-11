d64519833448285d62941d5bf00b74e0377b243e
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-10 16:27:50 -0800

    Well now that's kinda interesting

---
+++ esphome/components/toto_ir/select/water_position_select.cpp
@@ -0,0 +1,16 @@
+#include "water_position_select.h"
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace toto_ir {
+
+static const char *const TAG = "toto_ir.water_position_select";
+
+void TotoIRWaterPositionSelect::control(const std::string &value) {
+  this->publish_state(value);
+  this->parent_->set_water_position(value);
+}
+
+}  // namespace toto_ir
+}  // namespace esphome
