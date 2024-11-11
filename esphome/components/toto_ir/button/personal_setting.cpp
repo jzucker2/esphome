2a6b5d0b2f19ac356467a5e6c2f497d068991026
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-10 21:51:17 -0800

    Hoping this kinda compiles

--- esphome/components/toto_ir/button/personal_setting.cpp
+++ esphome/components/toto_ir/button/personal_setting.cpp
@@ -7,7 +7,7 @@ static const char *const TAG = "toto_ir.personal_setting_button";
 
 void PersonalSettingButton::press_action() {
   ESP_LOGI(TAG, "Sending personal setting request");
-  this->parent_->set_water_pressure("5");
+  this->parent_->set_water_pressure(this->get_water_pressure());
   this->parent_->set_water_position("5");
   this->parent_->set_water_temperature("3");
   this->parent_->set_seat_temperature("3");
--- esphome/components/toto_ir/button/personal_setting.h
+++ esphome/components/toto_ir/button/personal_setting.h
@@ -10,7 +10,7 @@ class PersonalSettingButton : public button::Button, public Parented<TotoIR> {
  public:
   PersonalSettingButton() = default;
   void set_water_pressure(const std::string &water_pressure) { this->water_pressure_ = water_pressure; }
-  std::string get_water_pressure_() const { return this->water_pressure_; }
+  std::string get_water_pressure() const { return this->water_pressure_; }
 
  protected:
   void press_action() override;
