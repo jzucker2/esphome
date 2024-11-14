13e244ce43e3f0a65e23324e59729f19a2555fa3
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-14 11:11:12 -0800

    One last try saving a config

--- esphome/components/toto_ir/button/personal_setting.cpp
+++ esphome/components/toto_ir/button/personal_setting.cpp
@@ -5,13 +5,22 @@ namespace toto_ir {
 
 static const char *const TAG = "toto_ir.personal_setting_button";
 
-void PersonalSettingButton::press_action() {
-  ESP_LOGD(TAG, "Sending personal setting request configuration");
-  const struct TotoConfig personal_config = {
+void PersonalSettingButton::setup() {
+  TotoConfig config = {
       this->get_water_pressure(),   this->get_water_position(),  this->get_water_temperature(),
       this->get_seat_temperature(), this->get_fan_temperature(),
   };
-  this->parent_->set_configuration(personal_config);
+  this->personal_config_ = config;
+}
+
+void PersonalSettingButton::press_action() {
+  ESP_LOGD(TAG, "Sending personal setting request configuration");
+  const struct TotoConfig config = {
+      this->personal_config_->water_pressure,    this->personal_config_->water_position,
+      this->personal_config_->water_temperature, this->personal_config_->seat_temperature,
+      this->personal_config_->fan_temperature,
+  };
+  this->parent_->set_configuration(config);
 }
 
 }  // namespace toto_ir
