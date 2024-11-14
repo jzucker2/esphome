0ea9da4628cda732fef4401732932941c52c6ada
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-14 11:03:13 -0800

    Now I just want it to work

--- esphome/components/toto_ir/button/personal_setting.cpp
+++ esphome/components/toto_ir/button/personal_setting.cpp
@@ -5,20 +5,13 @@ namespace toto_ir {
 
 static const char *const TAG = "toto_ir.personal_setting_button";
 
-void PersonalSettingButton::setup() {
-  TotoConfig config;
-  config.water_pressure = this->get_water_pressure();
-  config.water_position = this->get_water_position();
-  config.water_temperature = this->get_water_temperature();
-  config.seat_temperature = this->get_seat_temperature();
-  config.fan_temperature = this->get_fan_temperature();
-  this->personal_config_ = config;
-}
-
 void PersonalSettingButton::press_action() {
   ESP_LOGD(TAG, "Sending personal setting request configuration");
-  const TotoConfig config = this->personal_config_;
-  this->parent_->set_configuration(config);
+  const struct TotoConfig personal_config = {
+      this->get_water_pressure(),   this->get_water_position(),  this->get_water_temperature(),
+      this->get_seat_temperature(), this->get_fan_temperature(),
+  };
+  this->parent_->set_configuration(personal_config);
 }
 
 }  // namespace toto_ir
