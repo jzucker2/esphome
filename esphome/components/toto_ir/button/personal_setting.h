0e98794e7201fa40ac0a270652dcc5dda3859c89
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-14 10:08:10 -0800

    Oops, that was wrong

--- esphome/components/toto_ir/button/personal_setting.h
+++ esphome/components/toto_ir/button/personal_setting.h
@@ -19,9 +19,9 @@ class PersonalSettingButton : public button::Button, public Parented<TotoIR> {
   std::string get_fan_temperature() const { return this->fan_temperature_; }
   void set_seat_temperature(const std::string &seat_temperature) { this->seat_temperature_ = seat_temperature; }
   std::string get_seat_temperature() const { return this->seat_temperature_; }
+  void setup() override;
 
  protected:
-  void setup() override;
   void press_action() override;
   std::string water_pressure_{};
   std::string water_position_{};
