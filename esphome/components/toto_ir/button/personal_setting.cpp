b2d24694f4e26e19498ae989bbdd3008b4e2484f
Jordan Zucker <jordan.zucker@gmail.com>
2024-11-14 09:13:11 -0800

    Just want compilation

--- esphome/components/toto_ir/button/personal_setting.cpp
+++ esphome/components/toto_ir/button/personal_setting.cpp
@@ -7,13 +7,13 @@ static const char *const TAG = "toto_ir.personal_setting_button";
 
 void PersonalSettingButton::press_action() {
   ESP_LOGI(TAG, "Sending personal setting request configuration");
-  toto_ir::TotoConfig personal_config;
-  personal_config.water_pressure = this->get_water_pressure();
-  personal_config.water_position = this->get_water_position();
-  personal_config.water_temperature = this->get_water_temperature();
-  personal_config.seat_temperature = this->get_seat_temperature();
-  personal_config.fan_temperature = this->get_fan_temperature();
-  this->parent_->set_configuration(personal_config);
+  //  toto_ir::TotoConfig personal_config;
+  //  personal_config.water_pressure = this->get_water_pressure();
+  //  personal_config.water_position = this->get_water_position();
+  //  personal_config.water_temperature = this->get_water_temperature();
+  //  personal_config.seat_temperature = this->get_seat_temperature();
+  //  personal_config.fan_temperature = this->get_fan_temperature();
+  //  this->parent_->set_configuration(personal_config);
 }
 
 }  // namespace toto_ir
